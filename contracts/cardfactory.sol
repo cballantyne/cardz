2cb6a1a125b55b42a21c1df6f94b6a814b8b5b0d
Carl Ballantyne <carlballantyne@gmail.com>
2018-08-18 23:00:39 +1000

    Initial commit.

---
+++ contracts/cardfactory.sol
@@ -0,0 +1,56 @@
+pragma solidity ^0.4.19;
+
+import "./ownable.sol";
+
+contract CardFactory is Ownable {
+
+    event NewCard(uint cardId, string name, uint cardNum, uint instanceNum);
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+    uint cooldownTime = 1 days;
+
+    struct Card {
+        string name;
+        uint16 cardNum;
+        uint16 instanceNum;
+    }
+
+    Card[] public cards;
+
+    mapping (uint => address) public cardToOwner;
+    mapping (address => uint) ownerCardCount;
+
+    function _createCard(string _name, uint16 _cardNum, uint16 _instanceNum) internal {
+        uint id = cards.push(Card(_name, _cardNum, _instanceNum)) - 1;
+        cardToOwner[id] = msg.sender;
+        ownerCardCount[msg.sender]++;
+        NewCard(id, _name, _cardNum, _instanceNum);
+    }
+
+    function _generateRandomDna(string _str) private view returns (uint) {
+        uint rand = uint(keccak256(_str));
+        return rand % dnaModulus;
+    }
+
+    function mintCards(string _name) public {
+        // require(ownerZombieCount[msg.sender] == 0);
+        uint randDna = _generateRandomDna(_name);
+        _createCard(_name, 1, 1);
+        // randDna = randDna - randDna % 100;
+        // _createZombie(_name, randDna);
+    }
+
+    function getCardsByOwner(address _owner) external view returns(uint[]) {
+        uint[] memory result = new uint[](ownerCardCount[_owner]);
+        uint counter = 0;
+        for (uint i = 0; i < cards.length; i++) {
+            if (cardToOwner[i] == _owner) {
+                result[counter] = i;
+                counter++;
+            }
+        }
+        return result;
+    }
+
+}
---
+++ tradingcards.abi.js
@@ -0,0 +1,153 @@
+var tradingcardsAbi = [
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_name",
+                "type": "string"
+            }
+        ],
+        "name": "mintCards",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "name": "cardId",
+                "type": "uint256"
+            },
+            {
+                "indexed": false,
+                "name": "name",
+                "type": "string"
+            },
+            {
+                "indexed": false,
+                "name": "cardNum",
+                "type": "uint256"
+            },
+            {
+                "indexed": false,
+                "name": "instanceNum",
+                "type": "uint256"
+            }
+        ],
+        "name": "NewCard",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "name": "previousOwner",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "name": "newOwner",
+                "type": "address"
+            }
+        ],
+        "name": "OwnershipTransferred",
+        "type": "event"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "newOwner",
+                "type": "address"
+            }
+        ],
+        "name": "transferOwnership",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "cards",
+        "outputs": [
+            {
+                "name": "name",
+                "type": "string"
+            },
+            {
+                "name": "cardNum",
+                "type": "uint16"
+            },
+            {
+                "name": "instanceNum",
+                "type": "uint16"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "cardToOwner",
+        "outputs": [
+            {
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_owner",
+                "type": "address"
+            }
+        ],
+        "name": "getCardsByOwner",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256[]"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+            {
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
